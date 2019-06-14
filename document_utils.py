4bbd30e27637f1742803906b48ad5459e3ff22a0
Alexey Vasilev <alexey@vadebian>
2019-06-14 20:24:42 +0300

    Reindent with 4 spaces

--- document_utils.py
+++ document_utils.py
@@ -35,30 +35,30 @@ def zipdir(path, ziph):
 
 
 def unpack_docx(docx, folder):
-	if os.path.exists(folder):
-		raise ValueError(f'Folder {folder} is already exists')
+    if os.path.exists(folder):
+        raise ValueError(f'Folder {folder} is already exists')
 
-	os.mkdir(folder)
-	zip_ref = zipfile.ZipFile(docx, 'r')
-	zip_ref.extractall(folder)
-	zip_ref.close()
+    os.mkdir(folder)
+    zip_ref = zipfile.ZipFile(docx, 'r')
+    zip_ref.extractall(folder)
+    zip_ref.close()
 
 
 def replace_font_in_docx_xml(xml_name, old_font, new_font):
-	with open(XML_NAME) as f:
-    	xml_string = f.read()
+    with open(XML_NAME) as f:
+        xml_string = f.read()
 
-    if old_font not in xml_string:
-    	raise ValueError(f"Font {old_font} not fount in document")
+if old_font not in xml_string:
+    raise ValueError(f"Font {old_font} not fount in document")
 
-	xml_string = xml_string.replace(OLD_FONT, NEW_FONT)
+    xml_string = xml_string.replace(OLD_FONT, NEW_FONT)
 
-	with open(XML_NAME, 'w') as f:
-	    f.write(xml_string)
+    with open(XML_NAME, 'w') as f:
+        f.write(xml_string)
 
 
 def add_cant_split_to_tr(tr: ElementTree.Element):
-    
+
     if tr.tag != f"{NS_PREFIX}tr":
         raise ValueError("Function add_cant_split get non-tr tag or broken NAMESPACE")
     trpr = tr.find('w:trPr', NS)
@@ -71,39 +71,39 @@ def add_cant_split_to_tr(tr: ElementTree.Element):
 
 
 def disable_table_split_in_docx_xml(xml_name):
-	tree = ElementTree.parse(XML_NAME)
-	root = tree.getroot()
+    tree = ElementTree.parse(XML_NAME)
+    root = tree.getroot()
 
-    NAMESPACE = re.findall(r"\{(.*?)\}", root.tag)[0]
-    NS = {'w': NAMESPACE, }
-    NS_PREFIX = "{%s}" % NAMESPACE
-    body = root.find('w:body', NS)
-    table = body.find('w:tbl', NS)
-    for tr in table.findall('w:tr', NS):
-    	add_cant_split_to_tr(tr)
-    tree.write(XML_NAME)
+NAMESPACE = re.findall(r"\{(.*?)\}", root.tag)[0]
+NS = {'w': NAMESPACE, }
+NS_PREFIX = "{%s}" % NAMESPACE
+body = root.find('w:body', NS)
+table = body.find('w:tbl', NS)
+for tr in table.findall('w:tr', NS):
+    add_cant_split_to_tr(tr)
+tree.write(XML_NAME)
 
 
 def pack_docx(folder_from, docx_name):
-	if not os.path.exists(folder_from):
-		raise ValueError(f"Cant't found docx folder: {folder_from}")
-	if os.path.exists(docx_name):
-		raise ValueError(f"Docx file is already exists: {docx_name}")
+    if not os.path.exists(folder_from):
+        raise ValueError(f"Cant't found docx folder: {folder_from}")
+    if os.path.exists(docx_name):
+        raise ValueError(f"Docx file is already exists: {docx_name}")
 
-	zipf = zipfile.ZipFile(FORMATTED_FILE, 'w', zipfile.ZIP_DEFLATED)
-	with cd(WORD_TMP_DIR):
-		for item in os.listdir('.'):
-			if not os.path.isfile(os.path.join(item)):
-				zipdir(item, zipf)
-			else:
-				zipf.write(item)
-	zipf.close()
+    zipf = zipfile.ZipFile(FORMATTED_FILE, 'w', zipfile.ZIP_DEFLATED)
+    with cd(WORD_TMP_DIR):
+        for item in os.listdir('.'):
+            if not os.path.isfile(os.path.join(item)):
+                zipdir(item, zipf)
+            else:
+                zipf.write(item)
+    zipf.close()
 
 
 def format_rasp_docx(docx, mail_folder):
-	unpack_docx(docx, os.path.join(mail_folder,WORD_TMP_DIR))
-	replace_font_in_docx_xml(XML_NAME, OLD_FONT, NEW_FONT)
-	disable_table_split_in_docx_xml(XML_NAME)
-	pack_docx(FORMATTED_FILENAME, WORD_TMP_DIR)
-	shutil.remove_tree(os.path.join(mail_folder, WORD_TMP_DIR))
-	return FORMATTED_FILENAME
+    unpack_docx(docx, os.path.join(mail_folder,WORD_TMP_DIR))
+    replace_font_in_docx_xml(XML_NAME, OLD_FONT, NEW_FONT)
+    disable_table_split_in_docx_xml(XML_NAME)
+    pack_docx(FORMATTED_FILENAME, WORD_TMP_DIR)
+    shutil.remove_tree(os.path.join(mail_folder, WORD_TMP_DIR))
+    return FORMATTED_FILENAME
--- publish.py
+++ publish.py
@@ -25,53 +25,53 @@ class title_not_contains(object):
 
 
 def login_to_site():
-	driver = webdriver.Firefox()
-	driver.get(SITE_LOGIN_URL)
-	assert "Лотошино" in driver.title
-	name_input = driver.find_element_by_id('edit-name')
-	name_input.send_keys(SITE_USERNAME)
-	passwd_input = driver.find_element_by_id("edit-pass")
-	passwd_input.send_keys(SITE_PASSWORD)
-	passwd_input.send_keys(Keys.RETURN)
+    driver = webdriver.Firefox()
+    driver.get(SITE_LOGIN_URL)
+    assert "Лотошино" in driver.title
+    name_input = driver.find_element_by_id('edit-name')
+    name_input.send_keys(SITE_USERNAME)
+    passwd_input = driver.find_element_by_id("edit-pass")
+    passwd_input.send_keys(SITE_PASSWORD)
+    passwd_input.send_keys(Keys.RETURN)
 
-	wait = WebDriverWait(driver, 20)
-	element = wait.until(EC.title_contains(SITE_USERNAME))
+    wait = WebDriverWait(driver, 20)
+    element = wait.until(EC.title_contains(SITE_USERNAME))
 
 
 def load_jpegs_to_site(driver, folder, jpegs):
-	"""You must be logged in before uploading!"""
-	for filename in jpegs:
-		driver.get(SITE_FILEBROWSER_URL)
-		driver.find_element_by_name("upload").click()
-		file_load_input = driver.find_element_by_id("edit-imce")
-		file_load_input.send_keys(os.path.join(folder, filename))
-		driver.find_element_by_id("edit-upload").click()
-		element = wait.until(EC.presence_of_element_located((By.ID, filename)))
+    """You must be logged in before uploading!"""
+    for filename in jpegs:
+        driver.get(SITE_FILEBROWSER_URL)
+        driver.find_element_by_name("upload").click()
+        file_load_input = driver.find_element_by_id("edit-imce")
+        file_load_input.send_keys(os.path.join(folder, filename))
+        driver.find_element_by_id("edit-upload").click()
+        element = wait.until(EC.presence_of_element_located((By.ID, filename)))
     sleep(1)
 
 
 def create_rasp_html(jpegs):
-	HTML_TEMPLATE = """<p><img src="/sites/default/files/{}" alt="" width="849" height="1200" /></p>"""
-	html = ""
-	for jpeg in jpegs:
-		html += HTML_TEMPLATE.format(jpeg)
-	return html
+    HTML_TEMPLATE = """<p><img src="/sites/default/files/{}" alt="" width="849" height="1200" /></p>"""
+    html = ""
+    for jpeg in jpegs:
+        html += HTML_TEMPLATE.format(jpeg)
+    return html
 
 
 def update_rasp(driver, html):
-	driver.get(SITE_RASP_URL)
-	driver.find_element_by_id("wysiwyg-toggle-edit-body-und-0-value").click()
-	driver.find_element_by_id("edit-body-und-0-value").clear()
-	driver.find_element_by_id("edit-body-und-0-value").send_keys(HTML)
-	driver.find_element_by_id("edit-submit").click()
+    driver.get(SITE_RASP_URL)
+    driver.find_element_by_id("wysiwyg-toggle-edit-body-und-0-value").click()
+    driver.find_element_by_id("edit-body-und-0-value").clear()
+    driver.find_element_by_id("edit-body-und-0-value").send_keys(HTML)
+    driver.find_element_by_id("edit-submit").click()
 
-	wait.until(title_not_contains("Редактирование"))
-	sleep(1)
+    wait.until(title_not_contains("Редактирование"))
+    sleep(1)
 
 
 def publish_rasp(mail_folder, jpegs):
-	driver = login_to_site()
-	load_jpegs_to_site(driver, mail_folder, jpegs)
-	html = create_rasp_html(jpegs)
-	update_rasp(driver, html)
-	driver.close()
+    driver = login_to_site()
+    load_jpegs_to_site(driver, mail_folder, jpegs)
+    html = create_rasp_html(jpegs)
+    update_rasp(driver, html)
+    driver.close()
