6ff68cf6d7d85a6a312c38f44e485d197c07b617
Alexey Vasilev <alexey@vadebian>
2019-06-14 20:14:14 +0300

    Adding settings

--- settings.py
+++ settings.py
@@ -1,2 +1,9 @@
+from secrets import SITE_DOMAIN
+
 TMP_FOLDER = "/tmp"
 TMP_FOLDER_PREFIX = "autopublisher_"
+
+SITE_LOGIN_URL = f"http://{SITE_DOMAIN}/user"
+SITE_FILEBROWSER_URL = f"http://{SITE_DOMAIN}/imce"
+SITE_RASP_URL = f"http://{SITE_DOMAIN}/node/18/edit"
+SITE_NEWS_URL = f"http://{SITE_DOMAIN}/node/add/news"
