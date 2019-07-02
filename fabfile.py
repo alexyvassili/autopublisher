f957213d8c3f785d9e7a40d6a125c38bcc9db5bb
Alexey Vasilev <escantor@gmail.com>
2019-07-02 17:03:40 +0300

    Fixing sytemctl messages and adding jre

--- fabfile.py
+++ fabfile.py
@@ -89,7 +89,7 @@ def prepare_interpreter():
 
 
 def install_system_libs():
-    sudo('aptitude install -y imagemagick git xvfb x11-utils firefox-esr')
+    sudo('aptitude install -y imagemagick git xvfb x11-utils firefox-esr default-jre')
 
 
 def install_libreoffice():
