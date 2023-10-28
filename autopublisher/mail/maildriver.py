1b4229a8100b028ac429a64339c0ecd964dbef1a
Alexey Vasilev <escantor@gmail.com>
2023-10-28 16:44:13 +0300

    Update import

--- autopublisher/mail/maildriver.py
+++ autopublisher/mail/maildriver.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import shutil
-import mail
+import connection
 import prepare
 from document_utils import docx2html, get_text_from_html, unzip_without_structure, unrar
 from spelling import spell_line
