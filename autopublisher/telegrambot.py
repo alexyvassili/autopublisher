49f9187a60f3af9d3e4b0648a34920896a77019a
Alexey Vasilev <escantor@gmail.com>
2023-10-29 03:15:49 +0300

    Fix error message too long

--- autopublisher/telegrambot.py
+++ autopublisher/telegrambot.py
@@ -16,6 +16,9 @@ from autopublisher.secrets import BOT_TOKEN, BOT_PROXY
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+TELEGRAM_API_MESSAGE_LIMIT = 4096
+
+
 @owner_only
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, хозяин!")
@@ -51,6 +54,8 @@ def error(update, context):
     logging.warning('Update "%s" caused error "%s"', update, context.error)
     tbc = traceback.format_exc()
     context.bot.send_message(chat_id=update.effective_chat.id, text='Произошла ошибка!')
+    if len(tbc) > TELEGRAM_API_MESSAGE_LIMIT:
+        tbc = tbc[-TELEGRAM_API_MESSAGE_LIMIT:]
     context.bot.send_message(chat_id=update.effective_chat.id, text=tbc)
 
 
